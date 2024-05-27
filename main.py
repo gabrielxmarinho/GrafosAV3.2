a1e2a494e667e170f04ded5d95afd863cea5de28
Gabriel Xavier <gabrielxsmarinho@edu.unifor.br>
2024-05-27 20:38:14 -0300

    Adicionando novo Arquivo

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ conteudo=f.read().split("\n")
 vertices = conteudo[0].split(" ")
 arestas = []
 pesos = []
-for j in range(1,len(conteudo)):
+for j in range(1,len(conteudo)-1):
     valores = [conteudo[j].split(" ")[0],conteudo[j].split(" ")[1]]
     arestas.append(valores)
     pesos.append(int(conteudo[j].split(" ")[2]))
 G = Grafos.Grafo(vertices, arestas,pesos)
-G.Dijsktra("a")
+#Passo a última linha do conteúdo para o Algoritmo
+G.Dijsktra(conteudo[len(conteudo)-1])
 for vertice in G.V:
     print(f"Vertice: {vertice.dado}")
     if vertice.caminhoPonderado!=None:
